ceb1a7a9d9783affe3e497344a4407cfd0986793
abwwqqyy <qiyuewang0315@gmail.com>
2023-05-11 18:22:16 -0500

    OrderService Feign call PaymentService doPayment API

--- iOrderService/src/main/java/net/nvsoftware/iOrderService/service/OrderServiceImpl.java
+++ iOrderService/src/main/java/net/nvsoftware/iOrderService/service/OrderServiceImpl.java
@@ -2,8 +2,10 @@ package net.nvsoftware.iOrderService.service;
 
 import lombok.extern.log4j.Log4j2;
 import net.nvsoftware.iOrderService.entity.OrderEntity;
+import net.nvsoftware.iOrderService.external.client.PaymentServiceFeignClient;
 import net.nvsoftware.iOrderService.external.client.ProductServiceFeignClient;
 import net.nvsoftware.iOrderService.model.OrderRequest;
+import net.nvsoftware.iOrderService.model.PaymentRequest;
 import net.nvsoftware.iOrderService.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,9 @@ public class OrderServiceImpl implements OrderService{
 
     @Autowired
     private ProductServiceFeignClient productServiceFeignClient;
+
+    @Autowired
+    private PaymentServiceFeignClient paymentServiceFeignClient;
 
     @Override
     public long placeOrder(OrderRequest orderRequest) { // TODO: make this a transaction
@@ -34,11 +39,31 @@ public class OrderServiceImpl implements OrderService{
 
         orderEntity = orderRepository.save(orderEntity);
         log.info("OrderService placeOrder - save to orderdb done");
+
         // call ProductService to check quantity and reduce quantity if ok
         log.info("ProductServiceFeignClient reduceQuantity start.");
         productServiceFeignClient.reduceQuantity(orderRequest.getProductId(), orderRequest.getOrderQuantity());
         log.info("ProductServiceFeignClient reduceQuantity done.");
+
         // call PaymentService to charge paymentMode, mark order COMPLETED if success, else CANCELLED
+        log.info("PaymentServiceFeignClient doPayment start.");
+        PaymentRequest paymentRequest = PaymentRequest.builder()
+                .orderId(orderEntity.getOrderId())
+                .paymentMode(orderRequest.getPaymentMode())
+                .totalAmount(orderRequest.getTotalAmount())
+                .build();
+
+        String orderStatus = null;
+        try {
+            paymentServiceFeignClient.doPayment(paymentRequest);
+            orderStatus = "PLACED";
+        } catch (Exception e){
+            orderStatus = "PAYMENT_FAILED";
+        }
+
+        orderEntity.setOrderStatus(orderStatus);
+        orderRepository.save(orderEntity);
+        log.info("PaymentServiceFeignClient doPayment done.");
         return orderEntity.getOrderId();
     }
 }
