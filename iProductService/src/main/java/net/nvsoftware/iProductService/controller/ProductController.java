396fd788d08bfbb5d3cb6fc96e20c610c0f3885d
abwwqqyy <qiyuewang0315@gmail.com>
2023-05-10 22:33:05 -0500

    ProductService createProduct API

--- iProductService/src/main/java/net/nvsoftware/iProductService/controller/ProductController.java
+++ iProductService/src/main/java/net/nvsoftware/iProductService/controller/ProductController.java
@@ -1,10 +1,21 @@
 package net.nvsoftware.iProductService.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import net.nvsoftware.iProductService.model.ProductRequest;
+import net.nvsoftware.iProductService.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+    @PostMapping
+    public ResponseEntity<Long> createProduct(@RequestBody ProductRequest productRequest){
+        long productId = productService.createProduct(productRequest);
+        return new ResponseEntity<>(productId, HttpStatus.CREATED);
+    }
 }
